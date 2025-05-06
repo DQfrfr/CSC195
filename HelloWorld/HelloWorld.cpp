9493f579c7ab809f6db27a572fa9fae0d8b2eb33
DQfrfr <DJLove2005@yahoo.com>
2025-05-06 15:30:43 -0600

    Added a git message to HelloWorld Main file

--- HelloWorld/HelloWorld.cpp
+++ HelloWorld/HelloWorld.cpp
@@ -2,37 +2,8 @@
 #include <string>
 using namespace std;
 
-string checkVowel(char sum) {
-    switch (sum) {
-    case 'a':
-        return "*";
-    case 'e':
-        return "*";
-    case 'i' :
-        return "*";
-    case 'o':
-        return "*";
-    case 'u':
-        return "*";
-    default:
-        return string(1,sum);
-    }
-}
-
-string reverser(string sum) {
-    string awnser;
-
-    for (int i = sum.length() - 1; i >= 0; i--) {
-        awnser.append(checkVowel(sum[i]));
-    }
-    cout << awnser << endl;
-    return awnser;
-}
-
 int main() {
-    cout << "Enter a string to reverse and scramble: " << endl;
-    string stringToReverse;
-    cin >> stringToReverse;
-    cout << reverser(stringToReverse.) << endl;
+    cout << "Hello, World!" << endl;
+    cout << "Git is now tracking this project!" << endl;
     return 0;
 }
