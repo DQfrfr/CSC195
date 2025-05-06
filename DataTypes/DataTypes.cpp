02630251bea6749356ed3054ab7b115414b2caaf
DQfrfr <DJLove2005@yahoo.com>
2025-05-06 17:38:00 -0600

    Initial commit: basic payroll calculator using structs and vectors

---
+++ DataTypes/DataTypes.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <array>
+#include <vector>
+using namespace std;
+
+const float TAX = 0.1f;
+
+struct employee {
+    string name;
+    char initial;
+    short age;
+    bool isAdult;
+    unsigned int zipcode;
+    float wage;
+    short daysWorked;
+    float hoursWorkedPerDay[7];
+	float totalHours;
+};
+
+// [0] = grossIncome, [1] = taxAmount, [2] = netIncome
+vector<float> calculatePayroll(employee& emp) {
+	vector<float> payroll(3);
+	payroll[0] = emp.wage * emp.totalHours;
+	payroll[1] = payroll[0] * TAX;
+	payroll[2] = payroll[0] - payroll[1];
+	return payroll;
+}
+
+void printDetails(employee &emp) {
+	vector<float> payroll = calculatePayroll(emp);
+
+	cout << "\nEmployee Payroll Summary\n";
+	cout << "-------------------------\n";
+	cout << "Name: " << emp.name << " " << emp.initial << ".\n";
+	cout << "Age: " << emp.age << " (Adult: " << (emp.isAdult ? "Yes" : "No") << ")\n";
+	cout << "Zipcode: " << emp.zipcode << "\n";
+	cout << "Hourly Wage: $" << emp.wage << " per hour\n";
+	cout << "Total Hours Worked: " << emp.totalHours << " hours\n";
+	cout << "Gross Income: $" << payroll[0] << "\n";
+	cout << "Tax Amount: $" << payroll[1] << "\n";
+	cout << "Net Income: $" << payroll[2] << "\n";
+}
+
+void getDailyHoursWorked(employee &emp) {
+	emp.totalHours = 0;
+	for (int i = 0; i < emp.daysWorked; i++) {
+		cout << "Enter hours worked on day " << i + 1 << ": ";
+		cin >> emp.hoursWorkedPerDay[i];
+
+		emp.totalHours += emp.hoursWorkedPerDay[i];
+	}
+}
+
+//TOOD: Implement more rigerous input validation
+void collectDetails(employee &emp) {
+	cout << "Enter employee name: ";
+	cin >> emp.name;
+
+	cout << "Enter employee initial: ";
+	cin >> emp.initial;
+
+	cout << "Enter employee age: ";
+	cin >> emp.age;
+
+	cout << "Enter employee zipcode: ";
+	cin >> emp.zipcode;
+
+	cout << "Enter employee's hourly wage: ";
+	cin >> emp.wage;
+
+	cout << "Enter number of days worked (Max 7) ";
+	cin >> emp.daysWorked;
+
+	emp.isAdult = emp.age >= 18;
+}
+
+int main() {
+	employee emp;
+	collectDetails(emp);
+	getDailyHoursWorked(emp);
+	printDetails(emp);
+}
